e1cff4023c53515695f2de09134a2418946fd537
Anushka <anushkap161@gmail.com>
2023-10-04 00:55:12 +0530

    Body of Calculator

---
+++ app.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+let Calculator = () => {
+    return (<>
+  
+            <div id="CalculatorBody">
+                <div id="calculator-screen"></div>
+
+               <div id="calculator-button">
+               <div id="row1">
+                    <button className="button" id="7">7</button>
+                    <button className="button" id="8">8</button>
+                    <button className="button" id="9">9</button>
+                    <button className="button" id="divide">/</button>
+                </div>
+
+                <div id="row2">
+                    <button className="button" id="4">4</button>
+                    <button className="button" id="5">5</button>
+                    <button className="button" id="6">6</button>
+                    <button className="button" id="multiply">*</button>
+                </div>
+
+                <div id="row3">
+                    <button className="button" id="1">1</button>
+                    <button className="button" id="2">2</button>
+                    <button className="button" id="3">3</button>
+                    <button className="button" id="add">+</button>
+                </div>
+
+                <div id="row4">
+                    <button className="button" id="0">0</button>
+                    <button className="button" id="equal">=</button>
+                    <button className="button" id="clear">C</button>
+                    <button className="button" id="subtract">-</button>
+                </div>
+               </div>
+            </div>
+        </>
+    );
+}
+
+
+
+root.render(<Calculator />);
